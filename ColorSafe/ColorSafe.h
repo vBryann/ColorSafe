6072f6b95c6ca55db9b4b7e69348bb5fb823390c
vBryann <vitorbryan_melo@hotmail.com>
2021-07-23 11:13:11 -0300

    Initial Commit

---
+++ ColorSafe/ColorSafe.h
@@ -0,0 +1,18 @@
+//
+//  ColorSafe.h
+//  ColorSafe
+//
+//  Created by Vitor Bryan on 23/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ColorSafe.
+FOUNDATION_EXPORT double ColorSafeVersionNumber;
+
+//! Project version string for ColorSafe.
+FOUNDATION_EXPORT const unsigned char ColorSafeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorSafe/PublicHeader.h>
+
+
---
+++ ColorSafeTests/ColorSafeTests.m
@@ -0,0 +1,36 @@
+//
+//  ColorSafeTests.m
+//  ColorSafeTests
+//
+//  Created by Vitor Bryan on 23/07/21.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ColorSafeTests : XCTestCase
+
+@end
+
+@implementation ColorSafeTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
